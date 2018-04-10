f55ee3390a827186eb350641ae972141b3729f7c
Marc McKinnis <38083453+mnm5774@users.noreply.github.com>
2018-04-10 14:35:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+/* So I was not able to dynamically render a script tag and it work seamlessly.
+I'm thinking that having that ability will be helpful later on, but for general
+purpose js I think I need to put my code in a single file, so I don't mistakenly
+collide with other code. Maybe if I wrap things in a function expression I won't
+have to worry about collision. Will have to give more thought into this if
+I end up having a ton of code here later on.
+Might be able to solve the above by having an onload function in the HTML template. */
+
+
+//for sticky navbar
+// Get the navbar, using my library to create an object.
+var navbar = MM(".nav-container");
+
+// Get the offset position of the navbar
+var sticky = navbar.element.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navStuck() {
+  if (window.pageYOffset >= sticky) {
+    navbar.addClass("sticky")
+  } else {
+    navbar.removeClass("sticky");
+  }
+}
+
+// When the user scrolls the page, execute function to determine sticky.
+window.addEventListener ('scroll', navStuck);
+// When the page resizes, reset the sticky variable to be correct.
+window.addEventListener ('resize', function () {sticky = navbar.element.offsetTop});
+
+
---
+++ app/root-controllers.js
@@ -0,0 +1,76 @@
+//thirdly, a controller is invoked, which grabs the template, and content, and invokes render.
+var controllers = {
+	navbar : function (data, params) {
+		console.log (data);
+
+		var desktop = navbarTemplates.nav_desktop ();
+		var mobile = navbarTemplates.nav_mobile ();
+		var finalContent = desktop + mobile;
+
+		console.log ('content is grabbed, and ready to invoke render!');
+
+		utils.render ('navbar', finalContent);
+	},
+
+
+	home : function(data, params){
+		console.log (data);
+
+		//this looks like more of inserting the external data requested into our page.
+		//this will probably be useful later when dealing with a db or external API.
+	    /* var all_posts = JSON.parse(data);
+
+	    var posts_to_show = 3;
+	    var template_context = [];
+	    for (var i = 0; i < posts_to_show; i++){
+	        var post = all_posts[i];
+	        var item = {
+	            'link': '#post?'+post.post,
+	            'title': post.post.replace(/-/g, ' '),
+	            'snippet': post.snippet,
+	            'published_on': post.added_on,
+	        };
+	        template_context.push(item);
+	    } */
+
+	    //get recent posts
+	    //var recent_posts = templates.recent_posts(template_context);
+
+	    //get our template for this page. The more I think about it, the more I think
+	    //this is where we can grab multiple components for one request. We will see.
+	    var page1_home = page1Templates.home();
+
+	    //here is where we would probably add the items together to produce a final
+	    //content variable, with multiple objects inserted.
+	    //var page1_home = fullPage;  + recent_posts;
+	    console.log ('content is grabbed, and ready to invoke render!');
+
+	    //now actually render the html on the page. first argument is the id of the containing element
+	    //where the template will be inserted. second argument is the content that will be inserted.
+	    //this is where my naming convention for the html may be affected. Might need to adjust that.
+	    utils.render('body', page1_home);    
+
+
+	    //hero content-invoked after home because content should be priority. Plus this helps with
+	    //debugging; having them load separately.
+		var heroContent = page1Templates.hero ();
+
+		console.log ('hero is grabbed, and ready to invoke render!');
+
+		utils.render ('hero', heroContent);
+	},
+
+
+	footer : function (data, params) {
+		console.log (data);
+
+		var footerContent = footerTemplates.footer ();
+		var finalContent = footerContent;
+
+		console.log ('content is grabbed, and ready to invoke render!');
+
+		utils.render ('footer', finalContent);
+	}
+
+
+}
